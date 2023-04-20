973ad50c7638070cc3b96ec3de32990cc2cb6b1d
kartjom <jakubrutkowski3012@gmail.com>
2023-04-20 19:47:26 +0200

    velocity

--- LibraryInject/coduo_structs.h
+++ LibraryInject/coduo_structs.h
@@ -15,7 +15,8 @@ public:
 	int32_t time; //0x0010
 	char pad_0014[4]; //0x0014
 	Vector3 origin; //0x0018
-	char pad_0024[24]; //0x0024
+	Vector3 velocity; //0x0024
+	char pad_0030[12]; //0x0030
 	Vector3 viewangles; //0x003C
-	char pad_0048[797]; //0x0048
-};
+	char pad_0048[813]; //0x0048
+}; //Size: 0x0375
