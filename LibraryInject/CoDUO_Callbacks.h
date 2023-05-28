c9a5809d0650928c5fb3ad37ea30842825e8a4ee
kartjom <jakubrutkowski3012@gmail.com>
2023-05-28 14:28:14 +0200

    viewangles

--- LibraryInject/CoDUO_Callbacks.h
+++ LibraryInject/CoDUO_Callbacks.h
@@ -5,16 +5,14 @@
 
 namespace CoDUO::Gsc
 {
-	void Scr_GetCurrentDate();
-	void Scr_GetViewAngles(int);
+	void Scr_GetViewAngles(int entId);
 
 	inline std::unordered_map<std::string, gsc_function_t> gsc_functions
 	{
-		gsc_function("getdate", Scr_GetCurrentDate)
 	};
 
 	inline std::unordered_map<std::string, gsc_function_t> gsc_methods
 	{
-		gsc_function("getviewangles", Scr_GetViewAngles)
+		gsc_function("getviewangles", Scr_GetViewAngles),
 	};
 }
