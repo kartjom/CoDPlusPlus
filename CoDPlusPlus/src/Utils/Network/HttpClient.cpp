2b2b92d3162299daf6b78fa9c3d3bfea73064205
kartjom <jakubrutkowski3012@gmail.com>
2023-09-03 17:03:26 +0200

    http timeout

--- CoDPlusPlus/src/Utils/Network/HttpClient.cpp
+++ CoDPlusPlus/src/Utils/Network/HttpClient.cpp
@@ -3,6 +3,9 @@
 httplib::Result HttpClient::Get(std::string host, std::string endpoint)
 {
 	httplib::Client cli(host);
+	cli.set_read_timeout(15, 0);
+	cli.set_write_timeout(15, 0);
+
 	auto res = cli.Get(endpoint);
 
 	return res;
