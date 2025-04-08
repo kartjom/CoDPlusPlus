07b4d291385b1288fd341d1e56723eb2a36fda3a
kartjom <jakubrutkowski3012@gmail.com>
2025-04-08 09:53:40 +0200

    more level stuff

--- CoDPlusPlus/src/Structs/Engine/level_locals.h
+++ CoDPlusPlus/src/Structs/Engine/level_locals.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "stdint.h"
 
 struct level_locals_t
@@ -12,4 +12,14 @@ struct level_locals_t
 	int32_t framenum; //0x01E8
 	int32_t time; //0x01EC
 	int32_t previousTime; //0x01F0
+	char pad_01F4[36]; //0x01F4
+	int32_t numConnectedClients; //0x0218
+	int32_t sortedClients[64]; //0x021C
+	char voteString[1024]; //0x031C
+	char voteDisplayString[1024]; //0x071C
+	int32_t voteTime; //0x0B1C
+	char pad_0B20[4]; //0x0B20
+	int32_t voteYes; //0x0B24
+	int32_t voteNo; //0x0B28
+	int32_t numVotingClients; //0x0B2C
 };
